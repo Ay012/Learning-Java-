6e3de79d0f32d5e7e6224c8313c941f573031643
Ayan <ayantandon2109@gmail.com>
2025-12-21 10:06:07 +0530

    Escape sequences

--- escape.java
+++ escape.java
@@ -1,7 +1,10 @@
 public class escape {
     public static void main(String[] args){
-        System.out.println("Hello \"world\"");
-        System.out.println("Hello \nworld");
-        System.out.println("Hello \\ World \\");
+      //Patters using escape sequences
+        //Right half pyramid
+        System.out.println("*\n**\n***\n****\n*****");
+        //reverse right half
+        System.out.println("*****\n****\n***\n**\n*");
+        System.out.println("    *\n   **\n  ***\n ****\n*****");
     }
 }
